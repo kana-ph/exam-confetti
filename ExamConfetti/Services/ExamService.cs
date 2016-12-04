9e18d57dbe51a3b99908df409d25a9849f0edc56
kana0011 <kana.incognito@gmail.com>
2016-12-04 23:48:02 +0800

    Stub class for exam operations
    Signed-off-by: kana0011 <kana.incognito@gmail.com>

---
+++ ExamConfetti/Services/ExamService.cs
@@ -0,0 +1,24 @@
+﻿using ExamConfetti.Model;
+
+namespace ExamConfetti.Services
+{
+    class ExamService
+    {
+        private ExamService() { }
+
+        public static Exam ShuffleQuestions(Exam exam)
+        {
+
+        }
+
+        public static string Save(Exam exams)
+        {
+
+        }
+
+        public static Exam Load(string fileName)
+        {
+
+        }
+    }
+}
